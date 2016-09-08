7de77c07ed12e03feded4e4da42a835999b59ec5
Dmytro Rets <dmitryrets@gmail.com>
2016-09-08 02:50:49 +0200

    Implement binary search

---
+++ binary_search.py
@@ -0,0 +1,43 @@
+from math import floor
+
+def binary_search(a,t):
+    def binary_search(a,t,l,r):
+        if (l>r):
+            return -1
+        m = floor((l+r)/2)
+        if a[m]==t:
+            return m
+        elif a[m]>t:
+            return binary_search(a,t,l,m-1)
+        elif a[m]<t:
+            return binary_search(a,t,m+1,r)
+    return binary_search(a,t,0,len(a)-1)
+
+def binary_search_2(a,t):
+    l = 0
+    r = len(a) - 1
+    while l<=r:
+        m = floor((l+r)/2)
+        if a[m]==t:
+            return m
+        elif a[m]>t:
+            r=m-1
+        elif a[m]<t:
+            l=m+1
+    return -1
+
+lst = [1,3,5,7,9,11]
+lst.sort()
+
+assert binary_search(lst, -11) == binary_search_2(lst, -11) == -1
+assert binary_search(lst, 0) == binary_search_2(lst, 0) == -1
+assert binary_search(lst, 6) == binary_search_2(lst, 6) == -1
+assert binary_search(lst, 10000000000000000000000000000000000) == binary_search_2(lst, 10000000000000000000000000000000000) == -1
+assert binary_search(lst, -10000000000000000000000000000000000) == binary_search_2(lst, -10000000000000000000000000000000000) == -1
+
+assert binary_search(lst, 1) == binary_search_2(lst, 1) == 0
+assert binary_search(lst, 3) == binary_search_2(lst, 3) == 1
+assert binary_search(lst, 5) == binary_search_2(lst, 5) == 2
+assert binary_search(lst, 7) == binary_search_2(lst, 7) == 3
+assert binary_search(lst, 9) == binary_search_2(lst, 9) == 4
+assert binary_search(lst, 11) == binary_search_2(lst, 11) == 5
