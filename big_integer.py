ecf069b74e0c3592a7d46482eba00f23e45f5aad
Dmytro Rets <dmitryrets@gmail.com>
2016-11-05 11:47:16 +0100

    Add assertion to big_integer.

--- big_integer.py
+++ big_integer.py
@@ -1,14 +1,17 @@
-def output(a):
-    if a:
-        print(a.pop(),end="")
+def from_bi(a):
+    res=''
+    b=list(a)
+    if b:
+        res=str(b.pop())
     else:
-        print(0)
-    i=len(a)-1
+        res='0'
+    i=len(b)-1
     while i>=0:
-        print("%09d" % a[i],end="")
+        res+=str('{:>09}'.format(b[i]))
         i-=1
+    return res
 
-def read(s):
+def to_bi(s):
     a=[]
     i=len(s)
     while i>0:
@@ -25,6 +28,12 @@ def clean_zeros(a):
     while len(a)>1 and a[-1]==0:
         a.pop()
 
+s=1234567891011121314151617181920212223242526272829303132333343536373839404142
+a=to_bi(str(s))
+
+assert a==[839404142, 343536373, 303132333, 526272829, 212223242, 617181920, 121314151, 567891011, 1234]
+assert from_bi(a)==str(s)
+
 def add(a,b):
     base=1000*1000*1000
     carry=0
@@ -40,6 +49,8 @@ def add(a,b):
             carry=0
         i+=1
     return a
+
+assert str(s+s)==from_bi(add(to_bi(str(s)),to_bi(str(s))))
 
 def subtract(a,b):
     if len(a)==len(b):
@@ -61,6 +72,9 @@ def subtract(a,b):
     clean_zeros(a)
     return a
 
+m=4140393837363534333231302928272625242322212019181716151413121110987654321
+assert str(s-m)==from_bi(subtract(to_bi(str(s)),to_bi(str(m))))
+
 def multiply_on_short(a,b):
     base=1000*1000*1000
     assert b<base
@@ -76,6 +90,9 @@ def multiply_on_short(a,b):
     clean_zeros(a)
     return a
 
+n=3
+assert str(s*n)==from_bi(multiply_on_short(to_bi(str(s)),n))
+
 def multiply(a,b):
     base=1000*1000*1000
     c=(len(a)+len(b))*[0]
@@ -89,6 +106,8 @@ def multiply(a,b):
     clean_zeros(c)
     return c
 
+assert str(m*s)==from_bi(multiply(to_bi(str(m)),to_bi(str(s))))
+
 def devide_on_short(a,b):
     base=1000*1000*1000
     assert b<base
@@ -101,3 +120,6 @@ def devide_on_short(a,b):
         i-=1
     clean_zeros(a)
     return a
+
+h=411522630337040438050539060640070741080842090943101044111114512124613134714 # s/n
+assert str(h)==from_bi(devide_on_short(to_bi(str(s)),n))
